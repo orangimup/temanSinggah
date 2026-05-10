af717015bd20f75d718a8d0fc0e0e046dcac76fa
Mulyadisapusapu <kairulkumar50@gmail.com>
2026-05-10 10:27:31 +0700

    update month dropdown to show selected month in calendar router page

--- host/dashboard/scripts/calendar_router.js
+++ host/dashboard/scripts/calendar_router.js
@@ -6,11 +6,27 @@ fetch("/popups/month_dropdown.html")
     const monthPopupTrigger = document.querySelector(".header-filter");
     const monthPopup = document.querySelector(".dropdown-popup");
 
+    const monthOptions = document.querySelectorAll(".dropdown-option");
+
+    monthOptions.forEach((option) => {
+      option.addEventListener("click", () => {
+        const selectedMonthOption = option.textContent;
+
+        const filterValue = monthPopupTrigger.querySelector(".filter-value");
+
+        if (filterValue) {
+          filterValue.textContent = selectedMonthOption;
+        }
+
+        monthPopup.classList.remove("open");
+      });
+    });
+
     if (monthPopupTrigger && monthPopup) {
       monthPopupTrigger.addEventListener("click", (e) => {
         e.stopPropagation();
         monthPopup.classList.toggle("open");
-        
+
         const rect = monthPopupTrigger.getBoundingClientRect();
         monthPopup.style.top = `${rect.bottom + window.scrollY}px`;
         monthPopup.style.left = `${rect.left + window.scrollX}px`;
